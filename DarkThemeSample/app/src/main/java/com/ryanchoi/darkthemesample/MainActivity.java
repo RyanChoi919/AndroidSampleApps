f647375e4af7acfd6bacd968643ce6d8a410bd7c
Ryan Choi <choiyh919@gmail.com>
2021-11-04 13:03:00 +0900

    add new sample app

---
+++ DarkThemeSample/app/src/main/java/com/ryanchoi/darkthemesample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ryanchoi.darkthemesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
